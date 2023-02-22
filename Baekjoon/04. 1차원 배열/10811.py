5e4b95fcda2f3eafa60fc4a60d6fd0c50328bf8a
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-02-22 21:54:06 +0900

    Upload: Baekjoon ch4. solution
    백준 5단계 문제 풀이 파일 업로드
    > 완료
    - 10811.py

--- Baekjoon/04. 1차원 배열/10811.py
+++ Baekjoon/04. 1차원 배열/10811.py
@@ -3,8 +3,19 @@ bucket = [*range(1,N+1)]
 
 for _ in range(M):
     i,j = map(int,input().split())
-    for x in range(i,j):
-        temp = bucket[x-1]
-        bucket[x-1] = bucket[j-1-x]
-    bucket[j-1] = temp
+    front, change, back = bucket[:i-1], bucket[i-1:j], bucket[j:]
+    change.reverse()
+    bucket = front + change + back
+    
 print(' '.join(map(str,bucket)))
+
+
+'''
+f = lambda : map(int,input().split())
+N,M = f()
+bucket = [*range(1,n+1)]
+for _ in range(m):
+    i,j =f()
+    bucket[i-1:j] = bucket[i-1:j][::-1]
+print(*bucket)
+'''
