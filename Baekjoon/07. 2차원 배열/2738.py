c954a25ff7c801f61706cfab95e6c7a461ecca44
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-03-07 16:57:57 +0900

    Upload: Baekjoon ch7. solution
    백준 7단계 문제 풀이 파일 업로드
    > 완료
    - 2738.py

--- Baekjoon/07. 2차원 배열/2738.py
+++ Baekjoon/07. 2차원 배열/2738.py
@@ -1,7 +1,5 @@
-A, B = [],[]
 N,M = map(int,input().split())
-
-import numpy as np
+A,B = [], []
 
 for _ in range(N):
     A.append(list(map(int,input().split())))
@@ -9,8 +7,7 @@ for _ in range(N):
 for _ in range(N):
     B.append(list(map(int,input().split())))
 
-arr_A, arr_B = np.array(A),np.array(B)
-arr_A.reshape(N,M)
-arr_B.reshape(N,M)
-
-print(arr_A + arr_B)
+for i in range(N):
+    for j in range(M):
+        print(A[i][j] + B[i][j], end=' ')
+    print()
