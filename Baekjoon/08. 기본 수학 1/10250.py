63dedfe0eb1a85032e4e27ff6084f984e3f60317
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-03-10 16:06:02 +0900

    Update: 10250.py
    10250.py 풀이 과정 업데이트 (미완)

--- Baekjoon/08. 기본 수학 1/10250.py
+++ Baekjoon/08. 기본 수학 1/10250.py
@@ -1,5 +1,14 @@
 for _ in range(int(input())):
     H, W, N = map(int,input().split())
-    Y = str(N % W)
-    XX = f'0{N/W+1}' if N/W+1 <10 else str(N / W +1)
-    print(Y,XX)
+    row = list(range(H,H*W+1,H))
+    while row[i:=0] < N:
+        i += 1
+    j = N - row[i]
+
+    col = list(range(1,H+1))
+    Y = col[j-1]
+    XX = format(int(row[i]/H),'02')
+    print(str(Y)+str(XX))
+    # target = 2
+    # format(target, '03') >>> 002
+    # '{0:06d}'.format(target) >>> 000002
