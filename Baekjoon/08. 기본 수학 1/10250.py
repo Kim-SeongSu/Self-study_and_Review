34bac3f339fc26892565fb36176010a4acae9c46
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-03-14 17:19:30 +0900

    Upload: Baekjoon ch8. solution
    백준 8단계 10250.py 풀이 파일 업로드

--- Baekjoon/08. 기본 수학 1/10250.py
+++ Baekjoon/08. 기본 수학 1/10250.py
@@ -1,14 +1,29 @@
+'''
 for _ in range(int(input())):
     H, W, N = map(int,input().split())
     row = list(range(H,H*W+1,H))
-    while row[i:=0] < N:
+
+    i = 0
+    while row[i] < N:
         i += 1
-    j = N - row[i]
+    j = N - row[i] -1
+
 
     col = list(range(1,H+1))
-    Y = col[j-1]
+    Y = col[j]
     XX = format(int(row[i]/H),'02')
     print(str(Y)+str(XX))
+    
     # target = 2
     # format(target, '03') >>> 002
     # '{0:06d}'.format(target) >>> 000002
+'''    
+
+
+
+from sys import stdin
+input = stdin.readline
+
+for t in range(int(input())):
+    H, W, N = map(int, input().split())
+    print(((N-1)%H+1)*100+((N-1)//H+1))
