283d736ab0058e4233a14194457a2ad5974fac6a
Kim Seong-Su <104110605+Kim-SeongSu@users.noreply.github.com>
2023-03-23 18:39:06 +0900

    Create: Java folder & upload chap_01 study
    - Java 학습 폴더 생성
    - Java chap_01 폴더 및 파일 업로드
    > 01 HelloWorld, 02 자료형

---
+++ Java/chap_01/_01_HelloWorld.java
@@ -0,0 +1,10 @@
+package chap_01;
+
+// 출력
+public class _01_HelloWorld {
+        // psvm 또는 main 입력하면 자동완성!
+        public static void main (String[] args) {
+            // sout 입력하면 자동완성!
+            System.out.println("Hello World!!!");
+        }
+}
---
+++ Java/chap_01/_02_DataTypes.java
@@ -0,0 +1,26 @@
+package chap_01;
+
+// 자료형
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        // 문자열
+        System.out.println("Hello World");
+        System.out.println("안녕하세요");
+
+        // 숫자 자료형
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.14);
+
+        // 불리언 자료형
+        System.out.println(true);
+        System.out.println(false);
+
+        // 숫자 연산
+        System.out.println(1 + 2);
+        System.out.println(3 - 4);
+        System.out.println(5 * 6);
+        System.out.println(7 / 2); // 몫
+        System.out.println(7 % 2); // 나머지
+    }
+}
